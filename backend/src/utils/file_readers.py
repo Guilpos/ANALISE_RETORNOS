3b65777923bf4efbf9013347ce29dcd3bfff6cb9
Guilpos <guilherme.soucampos@gmail.com>
2026-09-01 13:37:50 -0300

    Teste de print

--- backend/src/utils/file_readers.py
+++ backend/src/utils/file_readers.py
@@ -73,6 +73,8 @@ def colunas_usadas(modelo, df: pd.DataFrame) -> pd.DataFrame:
         
     if modelo == "CONSIGFACIL_1":
         # 1. Extrai os nomes das colunas reais que estão escondidos na primeira linha (índice 0)
+        print(f'Como está df antes de filtrar?\n{df.head(10)}\n\n')
+
         nomes_colunas_conteudo = df.loc[0, 'Conteudo'].split(';')
         
         # 2. Divide a coluna 'Conteudo' usando o ';' e renomeia com os cabeçalhos extraídos
