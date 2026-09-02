8a14aec9fbcb39ef4bc915a6ab253cd8630f8a0f
Guilpos <guilherme.soucampos@gmail.com>
2026-09-02 17:33:10 -0300

    Removendo ponto final

--- backend/src/portais/Econsig_portal.py
+++ backend/src/portais/Econsig_portal.py
@@ -26,10 +26,11 @@ def processar_portal_econsig(df_bruto: pd.DataFrame, convenio: str, portal: str)
 
     # 2. Extração de texto da 'Crítica' (Sem limpar a moeda ainda!)
     mask_margem = df['Crítica'].fillna('').str.contains('MARGEM INSUFICIENTE. SERA INCLUIDO APENAS O VALOR DE')
+    
     if mask_margem.any():
         print('Encontradas críticas de margem insuficiente. Extraindo valores...')
         df['Valor Acatado'] = df.apply(
-            lambda row: row['Crítica'].split('VALOR DE R$ ')[1].split(' ')[0] if 'MARGEM INSUFICIENTE' in str(row['Crítica']) else row['Valor Acatado'],
+            lambda row: row['Crítica'].split('VALOR DE R$ ')[1].split(' ')[0].rstrip('.') if 'MARGEM INSUFICIENTE' in str(row['Crítica']) else row['Valor Acatado'],
             axis=1
         )
 
