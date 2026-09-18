16a6ef5203da86682275659a77fdbde54e10f67a
Guilherme Sousa Campos <guilherme.soucampos@gmail.com>
2026-09-18 13:43:40 -0300

    Como eu disse, estava errado. Faltou eu adicionar o portal no fim de base_portal.py

--- backend/src/portais/base_portal.py
+++ backend/src/portais/base_portal.py
@@ -2,7 +2,7 @@ import pandas as pd
 from utils.validators import validar_colunas_obrigatorias, validar_matematica_descontos, validar_chaves_nulas, validar_nome_portal_convenio
 from portais import Consigfacil_portal, Econsig_portal, Consigx_portal, Quantum_portal, Serha_portal, \
 Infoconsig_portal, Neoconsig_portal, Safeconsig_portal, Konexia_portal, Codata_portal, Sigrh_portal, \
-Asban_portal
+Asban_portal, Codiub_portal
 
 
 def decidir_layout_portal(portal: str, convenio: str, arquivo: pd.DataFrame) -> dict:
@@ -111,6 +111,8 @@ def decidir_layout_portal(portal: str, convenio: str, arquivo: pd.DataFrame) -> 
             resultado = Sigrh_portal.processar_portal_sigrh(arquivo, convenio=convenio, portal=portal)
         elif portal == 'ASBAN':
             resultado = Asban_portal.processar_portal_asban(arquivo, convenio=convenio, portal=portal)
+        elif portal == 'CODIUB':
+            resultado = Codiub_portal.processar_portal_codiub(arquivo, convenio=convenio, portal=portal)
         return resultado
     else:
         raise ValueError(f"Layout não definido para o portal '{portal}' e convênio '{convenio}'.")
